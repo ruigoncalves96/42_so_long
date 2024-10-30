c06d54668b5ad2f46e98a4b4452a5a1176f1c14e
Rui Gonçalves <ruigoncalves@Ruis-MacBook-Pro.local>
2024-10-30 14:03:37 +0000

    30/10 14:03

--- src/so_long.c
+++ src/so_long.c
@@ -6,23 +6,51 @@
 /*   By: randrade <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 18:15:10 by randrade          #+#    #+#             */
-/*   Updated: 2024/10/30 13:41:17 by randrade         ###   ########.fr       */
+/*   Updated: 2024/10/30 14:02:24 by randrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-/*	** TEST MLX **
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+//	** TEST MLX **
 void	ft_window(void)
 {
 	void	*mlx;
 	void	*mlx_win;
+	t_data	img;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
+//	void	*mlx;
+//	void	*mlx_win;
+//
+//	mlx = mlx_init();
+//	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+//	mlx_loop(mlx);
 }
-*/
+
 int	main(int argc, char *argv[])
 {
 	t_map_info	map_info;
